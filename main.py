b3e8379304125981a2c16c567078d638b18c5616
fastrick <charllez@hotmail.com>
2023-05-30 05:19:05 +0000

    clean main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import inquirer
 from lib.createRelation import relation_between_files, relation_between_texts 
 from lib.getText import get_text_from_xml
 from lib.gen_anki import generate_anki_deck
+
 folder_path = '/root/main/Subtitles/'
 
 # List all files in the folder
@@ -15,8 +16,7 @@ questions = [
               ),
 ]
 
-serie = inquirer.prompt(questions)
-selected_serie = serie['folder']
+selected_serie = inquirer.prompt(questions)['folder']
 
 
 season = os.listdir(f"{folder_path}/{selected_serie}")
@@ -28,8 +28,7 @@ questions = [
               ),
 ]
 
-season = inquirer.prompt(questions)
-selected_season = season['folder']
+selected_season = inquirer.prompt(questions)['folder']
 
 # Print the selected folder
 path_season = f"{folder_path}/{selected_serie}/{selected_season}"
@@ -38,12 +37,7 @@ subtitles = os.listdir(path_season)
 relational_files = relation_between_files(subtitles)
 
 for rel in relational_files:
-    sub0 = relational_files[rel][0]
-    sub1 = relational_files[rel][1]
-    #print(f"Comparing {sub0} with {sub1}")
     eng = get_text_from_xml(f"{path_season}/{relational_files[rel][0]}")
     pt = get_text_from_xml(f"{path_season}/{relational_files[rel][1]}")
     rela = relation_between_texts(eng, pt)
     generate_anki_deck(rela, selected_season, rel)
-    #subtitle = get_text_from_xml(f"{folder_path}/{selected_folder}/{relational_files[rel][1]}")
-    #print(subtitle)
